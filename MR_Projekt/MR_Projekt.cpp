b3a05ca224353d11ee967ffc166f691d04ea6685
MateuszRumin <rumin880@gmail.com>
2022-11-05 19:47:38 +0100

    v1.4

--- MR_Projekt/MR_Projekt.cpp
+++ MR_Projekt/MR_Projekt.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
    
-	int a, b, wynik;
+	int a, b,c, wynik;
 	cout << "Podaj a: ";
 	cin >> a;
 	cout << "Podaj b: ";
 	cin >> b;
+	cout << "Podaj c: ";
+	cin >> c;
 
 
 
@@ -24,7 +26,27 @@ int main()
 		wynik = (-b) / a;
 	}
 
-	cout << "Miejcem zerowym tej funkcji jest " << wynik;
+	cout << "Miejcem zerowym funkcji w postaci ax + b jest: " << wynik<<"\n";
+
+	if (a == 0)
+	{
+		wynik = (-b)-c;
+
+	}
+	else
+	{
+		wynik = ((-b) / a)-c;
+	}
+
+
+
+
+
+
+
+	cout << "Miejcem zerowym funkcji w postaci Ax + By + C = 0  jest: " << wynik << "\n";
+
+
 
 	return 0;
 }
