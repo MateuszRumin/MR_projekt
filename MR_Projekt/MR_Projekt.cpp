a5fca38337f7c137655fccdd9d3afe2767da033b
MateuszRumin <rumin880@gmail.com>
2022-11-05 21:02:49 +0100

    v2.2

--- MR_Projekt/MR_Projekt.cpp
+++ MR_Projekt/MR_Projekt.cpp
@@ -43,30 +43,47 @@ int main()
 	cout << "Miejcem zerowym funkcji liniowej w postaci Ax + By + C = 0 dla liczb a: " << a << " b: " << b <<" c: " <<c<< " jest: " << wynik << "\n";
 
 	
-
 	
-	delta = (b * b) - (4 * a * c);
-	if ( delta == 0)
-	{ 
-		wynik = (-b) / (2*a);
-			cout << "Miejcem zerowym funkcji kwadratowej w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " jest: " << wynik << "\n";
-	}
-	else if(delta > 0)
-	{ 
-		delta = sqrt(delta);
-		wynik = (((-b) - delta)) / (2 * a);
-		wynik2 = (((-b) + delta)) / (2 * a);
-
-		cout << "Miejscami zerowymi funkcji kwadratowej w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " jest: " << wynik << " oraz " << wynik2 << "\n";
 	
-	}
-	else if (delta < 0)
+	
+	if (a == 0)
 	{
-		cout << "Funkcji kwadratowa w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " nie posiada miejsc zerowych \n";
+		if (b == 0)
+		{
+			wynik = (-c);
+
+		}
+		else
+		{
+			wynik = (-c) / b;
+		}
+		cout << "Funkcja kwadratowej w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " jest funkcja liniowa bx + c a jej miejcem zerowym jest : " << wynik << "\n";
 	}
+	else
+	{
+
+		delta = (b * b) - (4 * a * c);
+		if (delta == 0)
+		{
+			wynik = (-b) / (2 * a);
+			cout << "Miejcem zerowym funkcji kwadratowej w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " jest: " << wynik << "\n";
+		}
+		else if (delta > 0)
+		{
+			delta = sqrt(delta);
+			wynik = (((-b) - delta)) / (2 * a);
+			wynik2 = (((-b) + delta)) / (2 * a);
+
+			cout << "Miejscami zerowymi funkcji kwadratowej w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " jest: " << wynik << " oraz " << wynik2 << "\n";
+
+		}
+		else if (delta < 0)
+		{
+			cout << "Funkcji kwadratowa w postaci ax^2 + bx + c dla liczb a: " << a << " b: " << b << " c: " << c << " nie posiada miejsc zerowych \n";
+		}
 
 
-
+	}
 
 
 
